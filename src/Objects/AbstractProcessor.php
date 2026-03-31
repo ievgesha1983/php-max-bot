01964d4a2122db7581dca310135ea919da7fd2f2
Evgenii Ivanov <ievgesha@mail.ru>
2026-03-31 15:12:30 +0700

    Add: functions to AbstractProcessor.php

--- src/Objects/AbstractProcessor.php
+++ src/Objects/AbstractProcessor.php
@@ -4,6 +4,26 @@ namespace EvgeshaFactory\PhpMaxBot\Objects;
 
 class AbstractProcessor
 {
+    public function switchCamelCaseToSnakeCase(string $string): string
+    {
+        return strtolower(preg_replace('#([a-z])([A-Z])#', '$1_$2', $string));
+    }
+
+    public function convertToMaxArray(): array
+    {
+        $convertedArray = array_map(
+            fn ($child) => is_object($child) ? $this->$child->convertToMaxArray() : $child,
+            get_object_vars($this)
+        );
+
+        $switchedKeys = array_map(
+            fn ($key) => $this->switchCamelCaseToSnakeCase($key),
+            array_keys($convertedArray)
+        );
+
+        return array_combine($switchedKeys, array_values($convertedArray));
+    }
+
     public function convertToArray(): array
     {
         $converted = [];
